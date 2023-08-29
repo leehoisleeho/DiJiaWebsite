485836aa6527f134466c2109864f7bea46378115
leeho <leehovip@gmail.com>
2023-08-29 10:14:15 +0800

    给联系我们加了动画

--- BackendWeb/src/pages/contact.vue
+++ BackendWeb/src/pages/contact.vue
@@ -1,17 +1,18 @@
 <script setup>
-import {ref,onMounted} from "vue";
+import {ref, onMounted} from "vue";
 import api from '../../API/api.js'
+
 const list = ref([])
-onMounted(()=>{
-  api.getContact().then(res=>{
+onMounted(() => {
+  api.getContact().then(res => {
     list.value = res.data
   })
 })
-const del = (id)=>{
+const del = (id) => {
   api.delContact({
     id
-  }).then(res=>{
-    api.getContact().then(res=>{
+  }).then(res => {
+    api.getContact().then(res => {
       list.value = res.data
     })
   })
@@ -28,21 +29,37 @@ const del = (id)=>{
         <li>联系人行业</li>
         <li>操作</li>
       </ul>
-      <ul class="tableinfo" v-for="item in list" :key="item.id">
-        <li>{{item.id}}</li>
-        <li>{{item.projectname}}</li>
-        <li>{{item.username}}</li>
-        <li>{{item.phone}}</li>
-        <li>{{item.industry}}</li>
-        <li>
-          <t-button theme="danger" @click="del(item.id)">删除</t-button>
-        </li>
-      </ul>
+      <TransitionGroup name="list" tag="div">
+        <ul class="tableinfo" v-for="item in list" :key="item.id">
+          <li>{{ item.id }}</li>
+          <li>{{ item.projectname }}</li>
+          <li>{{ item.username }}</li>
+          <li>{{ item.phone }}</li>
+          <li>{{ item.industry }}</li>
+          <li>
+            <t-button theme="danger" @click="del(item.id)">删除</t-button>
+          </li>
+        </ul>
+      </TransitionGroup>
     </div>
   </div>
 </template>
 <style scoped>
-.tableinfo{
+.list-enter-active,
+.list-leave-active {
+  transition: all 300ms ease;
+}
+
+.list-enter-from {
+  opacity: 0;
+  transform: translateY(-50px);
+}
+
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(50px);
+}
+.tableinfo {
   display: flex;
   height: 60px;
   align-items: center;
@@ -50,24 +67,28 @@ const del = (id)=>{
   margin-right: auto;
   border-bottom: 1px solid #efefef;
 }
-.tableTitle{
+
+.tableTitle {
   display: flex;
   height: 70px;
   align-items: center;
   font-weight: 600;
 }
-.tableinfo>li{
+
+.tableinfo > li {
   flex: 1;
   text-align: center;
   font-size: 13px;
   color: #333;
 }
-.tableTitle>li{
+
+.tableTitle > li {
   flex: 1;
   text-align: center;
   font-size: 15px;
   color: #333;
 }
+
 .infoBox {
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   width: 100%;
